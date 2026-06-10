3514a0bb58d3e342bcb46aa3fbd45ccea3ce9461
anilkumarthannedi-prog <anilkumarthannedi@gmail.com>
2026-06-10 07:40:55 +0530

    Update main.cpp

--- Task1_Number_Guessing_Game/main.cpp
+++ Task1_Number_Guessing_Game/main.cpp
@@ -1,25 +1,39 @@
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-int main(){
-  srand(time(0));
-  int randomNumber = rand() % 100+1;
-  int guess;
+int main() {
+    srand(time(0));
 
-  cout<<"guess a number between 1 and 100"<<endl;
-do{
-   cout<<"enter your guess:";
-  cin<<guess;
-  if(guess > randomNumber)
-    cout<<"too high"<<endl;
-  else if(guess < randomNumber)
-    cout<<"too low"<<endl;
-  else
-    cout<<"congragulations ! you guessed correctly"<<endl;
-}whilw(guess != randomNumber);
+    int randomNumber = rand() % 100 + 1;
+    int guess;
+    int attempts = 0;
 
-return 0;
+    cout << "==================================" << endl;
+    cout << "      NUMBER GUESSING GAME        " << endl;
+    cout << "==================================" << endl;
+    cout << "Guess a number between 1 and 100" << endl;
+
+    do {
+        cout << "\nEnter your guess: ";
+        cin >> guess;
+
+        attempts++;
+
+        if (guess > randomNumber) {
+            cout << "Too High! Try again." << endl;
+        }
+        else if (guess < randomNumber) {
+            cout << "Too Low! Try again." << endl;
+        }
+        else {
+            cout << "\nCongratulations! You guessed correctly!" << endl;
+            cout << "Number of attempts: " << attempts << endl;
+        }
+
+    } while (guess != randomNumber);
+
+    return 0;
 }
